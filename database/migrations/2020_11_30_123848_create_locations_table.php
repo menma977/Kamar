b578d672df913ef42d514ca7c54b9e813af4a977
MinAutumn <55562762+MinAutumn@users.noreply.github.com>
2020-11-30 12:54:22 +0700

    Update 2020_11_30_123848_create_locations_table.php

--- database/migrations/2020_11_30_123848_create_locations_table.php
+++ database/migrations/2020_11_30_123848_create_locations_table.php
@@ -15,6 +15,7 @@ class CreateLocationsTable extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
+            $table->text("address"); 
             $table->timestamps();
         });
     }
