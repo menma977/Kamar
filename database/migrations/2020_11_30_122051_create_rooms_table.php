c01bbeb5aada571c2585f166dc6c6674fe26a588
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-30 12:30:15 +0700

    x

--- database/migrations/2020_11_30_122051_create_rooms_table.php
+++ database/migrations/2020_11_30_122051_create_rooms_table.php
@@ -6,26 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRoomsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('rooms', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('rooms', function (Blueprint $table) {
+      $table->id();
+      $table->text("name");
+      $table->integer("price")->index();
+      $table->boolean("is_bond")->default(false);
+      $table->boolean("is_man")->default(true);
+      $table->date("join");
+      $table->date("end")->nullable();
+      $table->integer("item")->default(0);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('rooms');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('rooms');
+  }
 }
