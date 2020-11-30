e5eee6bbdceb84ab2612ca1472f083c5ab07d317
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-30 12:55:11 +0700

    Update 2020_11_30_123848_create_locations_table.php

--- database/migrations/2020_11_30_123848_create_locations_table.php
+++ database/migrations/2020_11_30_123848_create_locations_table.php
@@ -6,27 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateLocationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('locations', function (Blueprint $table) {
-            $table->id();
-            $table->text("address"); 
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('locations', function (Blueprint $table) {
+      $table->id();
+      $table->text("address");
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('locations');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('locations');
+  }
 }
