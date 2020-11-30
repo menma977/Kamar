5ca469027e400119f7bbe4bc5e64608815905ce4
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-30 14:10:36 +0700

    Update LocationController.php

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -3,47 +3,60 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\Location;
 use Auth;
+use Illuminate\Validation\ValidationException;
 
 class LocationController extends Controller
 {
-/**
- * @param Request $request
- * @return JsonResponse
- * @throws ValidationException
- */
-
-    public function store(Request $request, $isUpdate = false, $id = ""){
-        $this->validate($request,[
-            'address' => 'required|string'
-        ]);
-
-        if($isUpdate&&$id){
-            $location= Location::find($id);
-        }else {
-            $location = new Location();
-        }
-
-        $location->address = $request->address;
-        $location->save();
-
-        return response()->json([
-            'response' => 'Successfully add data'
-        ],200);
+  /**
+   * @param Request $request
+   * @param bool $isUpdate
+   * @param string $id
+   * @return JsonResponse
+   * @throws ValidationException
+   */
+  public function store(Request $request, $isUpdate = false, $id = "")
+  {
+    if (Auth::user()->role == 2) {
+      return response()->json([
+        'response' => 'Successfully add data'
+      ], 500);
     }
 
-    public function show()
-    {
-        if(Auth::user()->role =! 1){
-            $location = Location::where('id',Auth::user()->location)->get();
-        }else{
-            $location = Location::all();
-        }
-       
-        return response()->json([
-            'data' => $location,
-        ], 200);
+    $this->validate($request, [
+      'address' => 'required|string'
+    ]);
+
+    if ($isUpdate && $id) {
+      $location = Location::find($id);
+    } else {
+      $location = new Location();
     }
+
+    $location->address = $request->address;
+    $location->save();
+
+    return response()->json([
+      'response' => 'Successfully add data'
+    ], 200);
+  }
+
+  /**
+   * @return JsonResponse
+   */
+  public function show()
+  {
+    if (Auth::user()->role = !1) {
+      $location = Location::where('id', Auth::user()->location)->get();
+    } else {
+      $location = Location::all();
+    }
+
+    return response()->json([
+      'data' => $location,
+    ], 200);
+  }
 }
