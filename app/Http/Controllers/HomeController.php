23c22a2fb66b71040993091fa0fc7ba8d88977f8
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-04 12:21:07 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+  public function index(Request $request)
+  {
+    if ($request->now && $request->target) {
+      $now = Carbon::parse($request->now);
+      $target = Carbon::parse($request->target);
+    } else {
+      $now = Carbon::now()->subDays(30);
+      $target = Carbon::now()->addDay();
+    }
+
+    $room = Room::whereBetween('created_at', [$now, $target])->get();
+
+
+    $data = [
+      'room' => $room
+    ];
+
+    return view("dashboard", $data);
+  }
 }
